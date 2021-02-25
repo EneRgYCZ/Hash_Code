8a8b0b821d62f74582c453a327f2c844b4f64683
FcGod <36422731+FcGod@users.noreply.github.com>
2021-02-25 20:27:41 +0200

    V1

---
+++ Hash_Code_Problema/FcGod.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "FcGod.h"
+using namespace std;
+
+int aragaz(int x)
+{
+    return x;
+}
+
+int main()
+{
+
+    return 0;
+}
--- Hash_Code_Problema/Main.cpp
+++ Hash_Code_Problema/Main.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
 using namespace std;
 
+/*
+Input type:
+START: Duration - IntersectionsCount - StreetCount - CarCount - BonusPointsPerCar
+STREET: StartPos - EndPos - Name - CrossDuration
+CAR: StartPos - Path
+*/
+
+struct Roads{
+    int startPos, EndPos, CrossDuration;
+    string Name;
+    
+};
+
+struct Car{
+    Roads r;
+    int startPos;
+    int *path;
+
+};
+
+
+
+
+
+
+
 int main ()
 {
-    
+
+    return 0;
 }
